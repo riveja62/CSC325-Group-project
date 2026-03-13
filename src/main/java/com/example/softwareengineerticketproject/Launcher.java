414875932dea7dc7475fc9d12ce2d7e02233319f
Josep <riveja62@farmingdale.edu>
2026-03-12 20:28:04 -0400

    Ticket assignment again

--- src/main/java/com/example/softwareengineerticketproject/Launcher.java
+++ src/main/java/com/example/softwareengineerticketproject/Launcher.java
@@ -6,4 +6,4 @@ public class Launcher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
     }
-} //Hello dssdsdsddsfjnsdnjksnkjsdnck
+} //Hello
