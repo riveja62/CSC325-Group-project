45c838eeeb781eb9da7b1442b535e7846849ec1d
Nikolos Hurrinus <potatomasterme@gmail.com>
2026-04-09 19:51:20 -0400

    Refactored application and controller

--- src/main/java/com/example/softwareengineerticketproject/FirstController.java
+++ src/main/java/com/example/softwareengineerticketproject/FirstController.java
@@ -3,7 +3,7 @@ package com.example.softwareengineerticketproject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class FirstController {
     @FXML
     private Label welcomeText;
 
