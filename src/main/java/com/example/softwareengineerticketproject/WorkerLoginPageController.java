c189174daada551268041ec0d9521a224d951424
Nikolos Hurrinus <potatomasterme@gmail.com>
2026-05-09 23:47:39 -0400

    worker login complete, added controllers for homepages, and updated a little bit of formatting

--- src/main/java/com/example/softwareengineerticketproject/WorkerLoginPageController.java
+++ src/main/java/com/example/softwareengineerticketproject/WorkerLoginPageController.java
@@ -1,7 +1,11 @@
 package com.example.softwareengineerticketproject;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -16,6 +20,10 @@ methods:
 */
 
 public class WorkerLoginPageController {
+
+    // labels
+    @FXML
+    private Label errorLabel;
 
     // buttons
     @FXML
@@ -44,5 +52,59 @@ public class WorkerLoginPageController {
     private void registerButtonClicked() throws IOException{
         TicketManagerApplication.setRoot("WorkerRegisterPage");
     }
+
+    @FXML
+    private void loginButtonClicked() throws IOException{
+        if(login()){
+            TicketManagerApplication.setRoot("WorkerHomePage");
+        }
+    }
+
+    private boolean login() throws IOException {
+
+        if (usernameTextfield.getText().isEmpty() ||
+                passwordTextfield.getText().isEmpty()) {
+
+            errorLabel.setText("All fields must be filled out");
+            return false;
+        }
+
+
+        String username = usernameTextfield.getText();
+        String password = passwordTextfield.getText();
+
+        try {
+            // Query Firestore for username
+            ApiFuture<QuerySnapshot> future = TicketManagerApplication.fstore
+                    .collection("Workers")
+                    .whereEqualTo("username", username)
+                    .get();
+
+            QuerySnapshot snapshot = future.get();
+
+            if (snapshot.isEmpty()) {
+                errorLabel.setText("User not found");
+                return false;
+            }
+
+            DocumentSnapshot doc = snapshot.getDocuments().get(0);
+
+            String storedPassword = doc.getString("password");
+
+            if (storedPassword.equals(password)) {
+                System.out.println("Login successful!");
+            } else {
+                errorLabel.setText("Incorrect password");
+                return false;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            errorLabel.setText("Login error");
+            return false;
+        }
+        return true;
+    }
+
 
 }
