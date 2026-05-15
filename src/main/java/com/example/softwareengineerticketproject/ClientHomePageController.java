f47cd2e56940d2fb89c51f73f9e5ab118104fe5b
Nikolos Hurrinus <potatomasterme@gmail.com>
2026-05-14 20:09:59 -0400

    choice boxes now work

--- src/main/java/com/example/softwareengineerticketproject/ClientHomePageController.java
+++ src/main/java/com/example/softwareengineerticketproject/ClientHomePageController.java
@@ -1,16 +1,43 @@
 package com.example.softwareengineerticketproject;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 
 public class ClientHomePageController {
-    // assigns the username to label when this page starts
+
+    // assigns the username to label when this page starts and populates the choice boxes
     public void initialize(){
+        // assigns the label
         usernameLabel.setText(sessionUsername);
+
+        // populates device choice box
+        deviceChoiceBox.getItems().addAll("Phone", "Tablet", "Computer", "Other");
+
+        // populates issue choice box
+        issueChoiceBox.getItems().addAll("Lags", "Freezes", "Crashes", "Other");
+
+        // sets default choices
+        deviceChoiceBox.setValue("Device");
+        issueChoiceBox.setValue("Issue");
     }
+
+    // choice boxes
+    @FXML
+    private ChoiceBox<String> deviceChoiceBox;
+
+    @FXML
+    private ChoiceBox<String> issueChoiceBox;
+
+
+    // text fields
+    @FXML
+    private TextField subjectTextField;
+
+    // text area
+    @FXML
+    private TextArea descriptionTextArea;
 
     // labels
     @FXML
@@ -19,6 +46,9 @@ public class ClientHomePageController {
     // Buttons
     @FXML
     private Button logoutButton;
+
+    @FXML
+    private Button submitTicketButton;
 
     // session user variable
     private static String sessionUsername;
